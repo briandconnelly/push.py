fe3efa25232b7798fa63d9f87dd06c09fe8fe49d
Brian Connelly <bdc@bconnelly.net>
2014-11-01 13:10:26 -0700

    Put code in functions, fixes #11

--- push.py
+++ push.py
@@ -33,160 +33,168 @@ def valid_group_key(key):
 def valid_device_name(device):
     return re.match(r'^[A-Za-z0-9_-]{1,25}$', device) != None
 
+def parse_arguments():                                                          
+    """Parse command line arguments"""
 
-sound_choices = ['bike', 'bugle', 'cashregister', 'classical', 'cosmic',
-                 'falling', 'gamelan', 'incoming', 'intermission', 'magic',
-                 'mechanical', 'pianobar', 'siren', 'spacealarm', 'tugboat',
-                 'alien', 'climb', 'persistent', 'echo', 'updown', 'pushover',
-                 'none']
+    sound_choices = ['bike', 'bugle', 'cashregister', 'classical', 'cosmic', 'falling', 'gamelan', 'incoming', 'intermission', 'magic', 'mechanical', 'pianobar', 'siren', 'spacealarm', 'tugboat', 'alien', 'climb', 'persistent', 'echo', 'updown', 'pushover', 'none']
+
+    parser = argparse.ArgumentParser(prog=sys.argv[0],
+                                     description='Send a notification message with Pushover',
+                                     epilog='Available Sounds: {s}'.format(s=', '.join(sound_choices)))
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('-d', '--device', metavar='D', help='send message to' \
+                        ' specified device')
+    parser.add_argument('-t', '--title', metavar='t', help='message title')
+    parser.add_argument('-T', '--timestamp', metavar='T', help='send message ' \
+                        'specified UNIX timestamp', type=float)
+    parser.add_argument('-u', '--url', metavar='u',
+                        help='supplementary URL for message')
+    parser.add_argument('-U', '--urltitle', metavar='U', help='title for '\
+                        'supplementary url')
+    parser.add_argument('-s', '--sound', metavar='S', choices=sound_choices,
+                        default='pushover', help='play specified sound (see below)')
+    parser.add_argument('--request', action='store_true', default=False,
+                        help='print request token on success')
+
+    apigroup = parser.add_argument_group(title='Pushover API arguments (optional)',
+                                         description='Specify user or API token')
+    apigroup.add_argument('--user', help='Pushover user or group key')
+    apigroup.add_argument('--token', help='Application token')
+
+    pgroup = parser.add_argument_group(title='message priority (optional)',
+                                       description='By default, messages send'\
+                                       ' with normal priority.')
+    pgroup.add_argument('--silent', dest='priority', action='store_const',
+                        const=-2, help='send as lowest priority (-2)')
+    pgroup.add_argument('--quiet', dest='priority', action='store_const',
+                        const=-1, help='send as low priority (-1)')
+    pgroup.add_argument('--normal', dest='priority', action='store_const',
+                        const=0, help='send as normal priority (0)')
+    pgroup.add_argument('--high', dest='priority', action='store_const',
+                        const=1, help='send as high priority (1)')
+    pgroup.add_argument('--emergency', dest='priority', action='store_const',
+                        const=2, help='send as emergency priority, requiring '\
+                        'user confirmation (2)')
+    pgroup.add_argument('-r', '--retry', dest='retry', type=int,
+                        default=30, help='Retry interval (seconds) for '\
+                        'emergency messages (default: 30)')
+    pgroup.add_argument('-e', '--expire', dest='expire', type=int,
+                        default=3600, help='Expiration time (seconds) for '\
+                        'emergency messages (default: 3600)')
+
+    parser.add_argument('message', help='Message to send')
+    args = parser.parse_args()
+
+    return args
 
 
-parser = argparse.ArgumentParser(prog=sys.argv[0],
-                                 description='Send a notification message with Pushover',
-                                 epilog='Available Sounds: {s}'.format(s=', '.join(sound_choices)))
-parser.add_argument('--version', action='version', version=__version__)
-parser.add_argument('-d', '--device', metavar='D', help='send message to'\
-                    ' specified device')
-parser.add_argument('-t', '--title', metavar='t', help='message title')
-parser.add_argument('-T', '--timestamp', metavar='T', help='send message ' \
-                    'specified UNIX timestamp', type=float)
-parser.add_argument('-u', '--url', metavar='u', help='supplementary URL for message')
-parser.add_argument('-U', '--urltitle', metavar='U', help='title for '\
-                    'supplementary url')
-parser.add_argument('-s', '--sound', metavar='S', choices=sound_choices,
-                    default='pushover', help='play specified sound (see below)')
-parser.add_argument('--request', action='store_true', default=False,
-                    help='print request token on success')
+def main():
+    args = parse_arguments()
 
-apigroup = parser.add_argument_group(title='Pushover API arguments (optional)',
-                                     description='Specify user or API token')
-apigroup.add_argument('--user', help='Pushover user or group key')
-apigroup.add_argument('--token', help='Application token')
-
-pgroup = parser.add_argument_group(title='message priority (optional)',
-                                   description='By default, messages send '\
-                                   'with normal priority.')
-pgroup.add_argument('--silent', dest='priority', action='store_const',
-                    const=-2, help='send as lowest priority (-2)')
-pgroup.add_argument('--quiet', dest='priority', action='store_const',
-                    const=-1, help='send as low priority (-1)')
-pgroup.add_argument('--normal', dest='priority', action='store_const',
-                    const=0, help='send as normal priority (0)')
-pgroup.add_argument('--high', dest='priority', action='store_const', const=1,
-                    help='send as high priority (1)')
-pgroup.add_argument('--emergency', dest='priority', action='store_const',
-                    const=2, help='send as emergency priority, requiring '\
-                    'user confirmation (2)')
-pgroup.add_argument('-r', '--retry', dest='retry', type=int,
-                    default=30, help='Retry interval (seconds) for '\
-                    'emergency messages (default: 30)')
-pgroup.add_argument('-e', '--expire', dest='expire', type=int,
-                    default=3600, help='Expiration time (seconds) for '\
-                    'emergency messages (default: 3600)')
-
-parser.add_argument('message', help='Message to send')
-args = parser.parse_args()
-
-token = args.token
-if token is None:
-    token = os.environ.get('PUSHPY_TOKEN')
+    token = args.token
     if token is None:
-        print("Error: Must provide application token.")
-        sys.exit(11)
+        token = os.environ.get('PUSHPY_TOKEN')
+        if token is None:
+            print("Error: Must provide application token.")
+            sys.exit(11)
 
-if not valid_app_token(token):
-    print("Error: Invalid application token")
-    sys.exit(1)
+    if not valid_app_token(token):
+        print("Error: Invalid application token")
+        sys.exit(1)
 
-user = args.user
-if user is None:
-    user = os.environ.get('PUSHPY_USER')
+    user = args.user
     if user is None:
-        print("Error: Must provide application token.")
-        sys.exit(11)
+        user = os.environ.get('PUSHPY_USER')
+        if user is None:
+            print("Error: Must provide application token.")
+            sys.exit(11)
 
-if not valid_user_key(user):
-    print("Error: Invalid user/group key")
-    sys.exit(2)
+    if not valid_user_key(user):
+        print("Error: Invalid user/group key")
+        sys.exit(2)
 
-urlargs = {"user": user, "token": token}
+    urlargs = {"user": user, "token": token}
 
-if args.title:
-    if len(args.title) + len(args.message) > 512:
-        print("Error: Maximum length for title and message is 512 characters")
-        sys.exit(3)
-    urlargs['title'] = args.title
-else:
-    if len(args.message) > 512:
-        print("Error: Maximum length for title and message is 512 characters")
-        sys.exit(4)
-
-urlargs['message'] = args.message
-urlargs['priority'] = args.priority
-
-if args.priority == 2:
-    if args.retry < 30:
-        print("Error: Retry must be at least 30 seconds")
-        sys.exit(5)
-
-    if args.expire > 86400:
-        print("Error: Expire can not be larger than 86400 seconds")
-        sys.exit(6)
-
-    urlargs['retry'] = args.retry
-    urlargs['expire'] = args.expire
-
-urlargs['sound'] = args.sound
-
-if args.device:
-    if not valid_device_name(args.device):
-        print("Error: Invalid device name")
-        sys.exit(5)
-
-    urlargs['device'] = args.device
-
-
-if args.url:
-    urlargs['url'] = args.url
-
-if args.urltitle:
-    urlargs['urltitle'] = args.urltitle
-
-if args.timestamp:
-    urlargs['timestamp'] = args.timestamp
-
-
-conn = httplib.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json",
-urllib.urlencode(urlargs, {"Content-type":
-                           "application/x-www-form-urlencoded"}))
-response = conn.getresponse()
-data = response.read()
-
-try:
-    data_parsed = ast.literal_eval(data)
-except SyntaxError as s:
-    if args.request and response.status == 200:
-        print("Successfully sent message, however response could not be parsed")
-        sys.exit(6)
+    if args.title:
+        if len(args.title) + len(args.message) > 512:
+            print("Error: Maximum length for title and message is 512 characters")
+            sys.exit(3)
+        urlargs['title'] = args.title
     else:
-        print("Error: Message not sent, and could not parse response")
-        sys.exit(7)
+        if len(args.message) > 512:
+            print("Error: Maximum length for title and message is 512 characters")
+            sys.exit(4)
 
-if response.status == 200:
-    if args.request:
-        print(data_parsed['request'])
-    sys.exit(0)
-elif response.status == 429:
-    print("Error: message limit reached")
-    sys.exit(8)
-elif math.floor(response.status/100) == 4:
-    # TODO: handle the other 4xx errors
-    # https://pushover.net/api#friendly
-    pass
-else:
-    print("Error: Received status code {c}".format(c=data_parsed['status']))
-    for e in data_parsed['errors']:
-          print("Error: {e}".format(e=e))
-    sys.exit(9)
+    urlargs['message'] = args.message
+    urlargs['priority'] = args.priority
+
+    if args.priority == 2:
+        if args.retry < 30:
+            print("Error: Retry must be at least 30 seconds")
+            sys.exit(5)
+
+        if args.expire > 86400:
+            print("Error: Expire can not be larger than 86400 seconds")
+            sys.exit(6)
+
+        urlargs['retry'] = args.retry
+        urlargs['expire'] = args.expire
+
+    urlargs['sound'] = args.sound
+
+    if args.device:
+        if not valid_device_name(args.device):
+            print("Error: Invalid device name")
+            sys.exit(5)
+
+        urlargs['device'] = args.device
+
+
+    if args.url:
+        urlargs['url'] = args.url
+
+    if args.urltitle:
+        urlargs['urltitle'] = args.urltitle
+
+    if args.timestamp:
+        urlargs['timestamp'] = args.timestamp
+
+
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+    urllib.urlencode(urlargs, {"Content-type":
+                               "application/x-www-form-urlencoded"}))
+    response = conn.getresponse()
+    data = response.read()
+
+    try:
+        data_parsed = ast.literal_eval(data)
+    except SyntaxError as s:
+        if args.request and response.status == 200:
+            print("Successfully sent message, however response could not be parsed")
+            sys.exit(6)
+        else:
+            print("Error: Message not sent, and could not parse response")
+            sys.exit(7)
+
+    if response.status == 200:
+        if args.request:
+            print(data_parsed['request'])
+        sys.exit(0)
+    elif response.status == 429:
+        print("Error: message limit reached")
+        sys.exit(8)
+    elif math.floor(response.status/100) == 4:
+        # TODO: handle the other 4xx errors
+        # https://pushover.net/api#friendly
+        pass
+    else:
+        print("Error: Received status code {c}".format(c=data_parsed['status']))
+        for e in data_parsed['errors']:
+              print("Error: {e}".format(e=e))
+        sys.exit(9)
+
+
+if __name__ == "__main__":                                                      
+    main()
 
